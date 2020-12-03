b4f8153253684e78fe2401b8273ca1bac30cab60
1099446868 <1099446868@qq.com>
2020-12-03 20:23:36 +0800

    login

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// 加载全局样式
-import './styles/index.less'
-
 // 加载vant组件库
 import Vant from 'vant'
 // 加载vant样式
 import 'vant/lib/index.css'
+
+// 加载全局样式 (最好放在最后, 方便覆盖第三方样式)
+import './styles/index.less'
+
 // 全局注入vant组件
 
 // 加载自动设置rem基准值(html大小)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,33 +1,50 @@
 <template>
   <div class="login-container">
+    <!-- 子路由出口 -->
+    <router-view />
+    <!--/ 子路由出口 -->
     <van-nav-bar class="app-nav-bar" title="登录 / 注册" left-arrow @click-left="$router.back()"/>
     <!-- 登录表单 -->
-    <van-cell-group>
-      <van-field
-        v-model="user.mobile"
-        icon-prefix="toutiao"
-        left-icon="shouji"
-        placeholder="请输入手机号"
+    <van-form @submit="onLogin"
+    :show-error-message="false"
+    :show-error="false"
+    :validate-first="true"
+    @failed="onFailed">
+      <van-field v-model="user.mobile"
+      icon-prefix="toutiao"
+      left-icon="shouji"
+      placeholder="请输入手机号"
+      :rules="formRules.mobile"
       />
-      <van-field
-        v-model="user.code"
-        clearable
-        icon-prefix="toutiao"
-        left-icon="yanzhengma"
-        placeholder="请输入验证码">
+      <van-field v-model="user.code"
+      clearable icon-prefix="toutiao"
+      left-icon="yanzhengma"
+      placeholder="请输入验证码"
+      :rules="formRules.code">
         <template #button>
-          <van-button class="send-btn" size="small" round>发送验证码</van-button>
+          <van-button class="send-btn"
+          size="mini"
+          round
+          @click="onSendSms()"
+          >发送验证码</van-button>
         </template>
       </van-field>
-    </van-cell-group>
-    <div class='login-btn-wrap'>
-      <van-button class='login-btn' type="primary" block>登录</van-button>
-    </div>
+      <div class='login-btn-wrap'>
+        <van-button class='login-btn' type="primary" native-type="submit" block>登录</van-button>
+      </div>
+    </van-form>
     <!--/ 登录表单 -->
+    <div class="default-user-wrap">
+      <p>测试账号: 17090086870</p>
+      <p>测试密码: 246810</p>
+    </div>
   </div>
 </template>
 
 <script>
+import { login } from '@/api/user'
+// import { Toast } from 'vant'
+
 export default {
   name: 'LoginIndex',
   components: {},
@@ -37,13 +54,53 @@ export default {
       user: {
         mobile: '',
         code: ''
+      },
+      formRules: {
+        mobile: [
+          { required: true, message: '请输入手机号' },
+          { pattern: /^1[1-9]\d{9}$/, message: '手机号格式错误' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '验证码格式错误' }
+        ]
       }
     }
   },
   computed: {},
   watch: {},
   created () {},
-  methods: {}
+  methods: {
+    async onLogin () {
+      // Toast.loading({
+      this.$toast.loading({
+        message: '登录中...',
+        forbidClick: true, // 禁用背景点击
+        duration: 0 // 展示时长(ms)，值为 0 时，toast 不会消失
+      })
+      try {
+        const res = await login(this.user)
+        console.log(res)
+        this.$toast.success('登陆成功')
+      } catch (err) {
+        console.log(err)
+        this.$toast.fail('登陆失败')
+      }
+    },
+
+    onFailed (error) {
+      if (error.errors[0]) {
+        this.$toast({
+          message: error.errors[0].message,
+          position: 'top'
+        })
+      }
+    },
+
+    onSendSms () {
+      // 发送验证码
+    }
+  }
 }
 </script>
 
@@ -66,6 +123,14 @@ export default {
       .van-button__text {
         font-size: 15px;
       }
+    }
+  }
+  .default-user-wrap {
+    width: 80%;
+    margin: 0 auto;
+    p {
+      font-size: 15px;
+      color: #c5c5c5;
     }
   }
 }
